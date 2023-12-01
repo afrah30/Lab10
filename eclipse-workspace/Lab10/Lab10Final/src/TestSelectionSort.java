34ae1c3c40d87d7b0901fa712d2d21384d67c5ad
Afrah <afrahm@umich.edu>
2023-11-30 20:30:56 -0500

    changes made to m and d tests

--- eclipse-workspace/Lab10/Lab10Final/src/TestSelectionSort.java
+++ eclipse-workspace/Lab10/Lab10Final/src/TestSelectionSort.java
@@ -3,16 +3,16 @@ import org.junit.Test;
 public class TestSelectionSort {
 
   @Test
-public void testMixed() {
-  int[] arr = { 5, -2, 0, 1, -5 };
-  SelectionSort.basicSelectionSort(arr);
-  assertArrayEquals(new int[] { -5, -2, 0, 1, 5 }, arr);
-}
+  public void testMixed() {
+      int[] arr = { 5, -2, 0, 1, -5 };
+      SelectionSort.basicSelectionSort(arr);
+      assertArrayEquals(new int[] { -5, -2, 0, 1, 5 }, arr);
+  }
 
-@Test
-public void testDuplicates() {
-  int[] arr = { 5, 2, 5, 1, 2 };
-  SelectionSort.basicSelectionSort(arr);
-  assertArrayEquals(new int[] { 1, 2, 2, 5, 5 }, arr);
-}
+  @Test
+  public void testDuplicates() {
+      int[] arr = { 5, 2, 5, 1, 2 };
+      SelectionSort.basicSelectionSort(arr);
+      assertArrayEquals(new int[] { 1, 2, 2, 5, 5 }, arr);
+  }
 }
