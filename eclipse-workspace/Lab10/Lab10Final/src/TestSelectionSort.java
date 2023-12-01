dc1700aafbbba79aaf9c832d3e497071ec83ef16
Afrah <afrahm@umich.edu>
2023-11-30 20:19:53 -0500

    final changes made to pos and neg tests

--- eclipse-workspace/Lab10/Lab10Final/src/TestSelectionSort.java
+++ eclipse-workspace/Lab10/Lab10Final/src/TestSelectionSort.java
@@ -15,18 +15,4 @@ public class TestSelectionSort {
         SelectionSort.basicSelectionSort(arr);
         assertArrayEquals(new int[] { -9, -5, -5, -2, -1 }, arr);
     }
-
-    @Test
-    public void testMixed() {
-        int[] arr = { 5, -2, 0, 1, -5 };
-        SelectionSort.basicSelectionSort(arr);
-        assertArrayEquals(new int[] { -5, -2, 0, 1, 5 }, arr);
-    }
-
-    @Test
-    public void testDuplicates() {
-        int[] arr = { 5, 2, 5, 1, 2 };
-        SelectionSort.basicSelectionSort(arr);
-        assertArrayEquals(new int[] { 1, 2, 2, 5, 5 }, arr);
-    }
 }
