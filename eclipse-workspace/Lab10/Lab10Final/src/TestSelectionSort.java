79fe447c5999db5832a35dd7e160a52a477c207c
Afrah <afrahm@umich.edu>
2023-11-30 20:15:52 -0500

    positives and negatives test changes made

--- eclipse-workspace/Lab10/Lab10Final/src/TestSelectionSort.java
+++ eclipse-workspace/Lab10/Lab10Final/src/TestSelectionSort.java
@@ -1,20 +1,19 @@
 import static org.junit.Assert.assertArrayEquals;
 import org.junit.Test;
-
 public class TestSelectionSort {
 
     @Test
     public void testPositives() {
-        int[] arr = {10, 7, 3, 8, 1}; // Change positive values
+        int[] arr = { 5, 2, 9, 1, 5 };
         SelectionSort.basicSelectionSort(arr);
-        assertArrayEquals(new int[]{1, 3, 7, 8, 10}, arr);
+        assertArrayEquals(new int[] { 1, 2, 5, 5, 9 }, arr);
     }
 
     @Test
     public void testNegatives() {
-        int[] arr = {-3, -8, -1, -5, -2}; // Change negative values
+        int[] arr = { -5, -2, -9, -1, -5 };
         SelectionSort.basicSelectionSort(arr);
-        assertArrayEquals(new int[]{-8, -5, -3, -2, -1}, arr);
+        assertArrayEquals(new int[] { -9, -5, -5, -2, -1 }, arr);
     }
 
     @Test
